cb87151fcf6eef5f613991eb1080a9c6e1f64463
wubbalubbaaa <543668641@qq.com>
2021-12-14 22:36:03 +0800

    feat: support kqueue

--- poller_epoll.go
+++ poller_epoll.go
@@ -3,10 +3,8 @@
 package easyNet
 
 import (
-	"errors"
 	"io"
 	"log"
-	"net"
 	"sync/atomic"
 	"syscall"
 	"unsafe"
@@ -31,9 +29,6 @@ type poller struct {
 	readBuffer []byte
 
 	pollType string
-
-	twRead  *timerWheel
-	twWrite *timerWheel
 }
 
 func (p *poller) online() int64 {
@@ -191,13 +186,6 @@ func (p *poller) start() {
 					p.readWrite(&events[i])
 				}
 			}
-
-			// now := time.Now()
-			// msec = p.twRead.check(now)
-			// twout = p.twWrite.check(now)
-			// if twout < msec {
-			// 	msec = twout
-			// }
 		}
 	}
 }
@@ -214,7 +202,7 @@ func (p *poller) modWrite(fd int) error {
 	return syscall.EpollCtl(p.epfd, syscall.EPOLL_CTL_MOD, fd, &syscall.EpollEvent{Fd: int32(fd), Events: syscall.EPOLLRDHUP | syscall.EPOLLIN | syscall.EPOLLOUT})
 }
 
-func (p *poller) deleteEvent(fd int) error {
+func (p *poller) deleteWrite(fd int) error {
 	return syscall.EpollCtl(p.epfd, syscall.EPOLL_CTL_DEL, fd, &syscall.EpollEvent{Fd: int32(fd)})
 }
 
@@ -296,151 +284,7 @@ func newPoller(g *Gopher, isListener bool, index int) (*poller, error) {
 		p.pollType = "listener"
 	} else {
 		p.pollType = "poller"
-
-		p.twRead = newTimerWheel(int(maxTimeout/interval)+1, interval, errReadTimeout)
-		p.twRead.start()
-		p.twWrite = newTimerWheel(int(maxTimeout/interval)+1, interval, errWriteTimeout)
-		p.twWrite.start()
 	}
 
 	return p, nil
-}
-
-func sockaddrToAddr(sa syscall.Sockaddr) net.Addr {
-	var a net.Addr
-	switch sa := sa.(type) {
-	case *syscall.SockaddrInet4:
-		a = &net.TCPAddr{
-			IP:   append([]byte{}, sa.Addr[:]...),
-			Port: sa.Port,
-		}
-	case *syscall.SockaddrInet6:
-		var zone string
-		if sa.ZoneId != 0 {
-			if ifi, err := net.InterfaceByIndex(int(sa.ZoneId)); err == nil {
-				zone = ifi.Name
-			}
-		}
-		a = &net.TCPAddr{
-			IP:   append([]byte{}, sa.Addr[:]...),
-			Port: sa.Port,
-			Zone: zone,
-		}
-	case *syscall.SockaddrUnix:
-		a = &net.UnixAddr{Net: "unix", Name: sa.Name}
-	}
-	return a
-}
-
-func getSockaddr(proto, addr string) (sa syscall.Sockaddr, soType int, err error) {
-	var tcp *net.TCPAddr
-
-	tcp, err = net.ResolveTCPAddr(proto, addr)
-	if err != nil && tcp.IP != nil {
-		return nil, -1, err
-	}
-
-	tcpVersion, err := determineTCPProto(proto, tcp)
-	if err != nil {
-		return nil, -1, err
-	}
-
-	switch tcpVersion {
-	case "tcp":
-		return &syscall.SockaddrInet4{Port: tcp.Port}, syscall.AF_INET, nil
-	case "tcp4":
-		sa := &syscall.SockaddrInet4{Port: tcp.Port}
-
-		if tcp.IP != nil {
-			copy(sa.Addr[:], tcp.IP[12:16])
-		}
-
-		return sa, syscall.AF_INET, nil
-	case "tcp6":
-		sa := &syscall.SockaddrInet6{Port: tcp.Port}
-
-		if tcp.IP != nil {
-			copy(sa.Addr[:], tcp.IP)
-		}
-
-		if tcp.Zone != "" {
-			iface, err := net.InterfaceByName(tcp.Zone)
-			if err != nil {
-				return nil, -1, err
-			}
-
-			sa.ZoneId = uint32(iface.Index)
-		}
-
-		return sa, syscall.AF_INET6, nil
-	}
-
-	return nil, -1, errors.New("unsupported protocol")
-}
-
-func determineTCPProto(proto string, ip *net.TCPAddr) (string, error) {
-	if ip.IP.To4() != nil {
-		return "tcp4", nil
-	}
-
-	if ip.IP.To16() != nil {
-		return "tcp6", nil
-	}
-
-	switch proto {
-	case "tcp", "tcp4", "tcp6":
-		return proto, nil
-	}
-
-	return "", errors.New("unsupported protocol")
-}
-
-func listen(network, address string, backlogNum int64) (int, error) {
-	var (
-		err        error
-		soType, fd int
-		sockaddr   syscall.Sockaddr
-	)
-
-	if sockaddr, soType, err = getSockaddr(network, address); err != nil {
-		return -1, err
-	}
-
-	syscall.ForkLock.RLock()
-	defer syscall.ForkLock.RUnlock()
-	if fd, err = syscall.Socket(soType, syscall.SOCK_STREAM, syscall.IPPROTO_TCP); err != nil {
-		return -1, err
-	}
-
-	if err = syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, syscall.SO_REUSEADDR, 1); err != nil {
-		syscall.Close(fd)
-		return -1, err
-	}
-
-	socketOptReusePort := 0x0F
-	if err = syscall.SetsockoptInt(fd, syscall.SOL_SOCKET, socketOptReusePort, 1); err != nil {
-		syscall.Close(fd)
-		return -1, err
-	}
-
-	if err = syscall.Bind(fd, sockaddr); err != nil {
-		syscall.Close(fd)
-		return -1, err
-	}
-
-	n := int(backlogNum)
-	if backlogNum <= 0 {
-		n = syscall.SOMAXCONN
-	}
-	if err = syscall.Listen(fd, n); err != nil {
-		syscall.Close(fd)
-		return -1, err
-	}
-
-	if err = syscall.SetNonblock(fd, true); err != nil {
-		syscall.Close(fd)
-		return -1, err
-	}
-
-	return fd, nil
 }
